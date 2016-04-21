8333c246492974bb2d118ea091c0bc6208234e87
Andreas Sefzig <andreas@sefzig.net>
2016-04-21 13:54:24 +0200

    Tests

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const _ = require('lodash');
 const Script = require('smooch-bot').Script;
+const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
@@ -31,6 +33,76 @@ module.exports = new Script({
                 .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
                         'teach me how to do anything else!'))
                 .then(() => 'finish');
+        }
+    }
+},{
+    processing: {
+        //prompt: (bot) => bot.say('Beep boop...'),
+        receive: () => 'processing'
+    },
+
+    start: {
+        receive: (bot) => {
+            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+                .then(() => 'speak');
+        }
+    },
+
+    speak: {
+        receive: (bot, message) => {
+
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split(/(<img src=\'[^>]*\'\/>)/);
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    if (!line.startsWith("<")) {
+                        p = p.then(function() {
+                            return bot.say(line);
+                        });
+                    } else {
+                        // p = p.then(function() {
+                        //     var start = line.indexOf("'") + 1;
+                        //     var end = line.lastIndexOf("'");
+                        //     var imageFile = line.substring(start, end);
+                        //     return bot.sendImage(imageFile);
+                        // });
+                    }
+                })
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
         }
     }
 });
