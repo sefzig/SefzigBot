1250b5d481eff2e88cfd72c3d99bef55b6f7c0c0
Andreas Sefzig <andreas@sefzig.net>
2016-04-21 13:35:34 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SefzigBot
+
+Andreas Sefzigs Bot (basierend auf [smooch/smooch-bot](https://github.com/smooch/smooch-bot) und verfeinert mit [esthercrawford/EstherBot](https://github.com/esthercrawford/EstherBot))
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/sefzig/SefzigBot)
---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use("/css", express.static(__dirname + '/views/css'));
+app.use("/js", express.static(__dirname + '/views/js'));
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        appToken: process.env.SMOOCH_APP_TOKEN
+    });
+});
+
+module.exports = app;
