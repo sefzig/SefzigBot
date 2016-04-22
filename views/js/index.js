5a7741a1eee57c477bac4332ca2f911f6853faf2
Andreas Sefzig <andreas@sefzig.net>
2016-04-22 15:01:17 +0200

    Tests

--- views/js/index.js
+++ views/js/index.js
@@ -39,13 +39,13 @@
              var text_neu = text;
              var texte = text.split("--");
              
-             for (i = 1; i < texte.length; i++) {
+             for (i = 1; i = texte.length; i++) {
                 
              // var befehle = texte[i].split(/,|;|\.|!|?| /);
                 var befehle = texte[i].split(" "); 
-                if (befehle[1]) { var befehl = befehle[0]; } else { var befehl = texte[i]; }
+                if ((befehle[1]) && (befehle[1] != "")) { var befehl = befehle[0]; } else { var befehl = texte[i]; }
                 
-                text_neu = text_neu.replace("--"+befehl, '<span class="befehl" style="background-color:red">--'+befehl+'</span>');
+                text_neu = text_neu.replace("--"+befehl, '<span class="befehl">--'+befehl+'</span>');
                 
              }
              
@@ -53,7 +53,7 @@
              
           });
           
-       },333);
+       },1000);
        
     });
     
