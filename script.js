5dc50930ee9700648359a6ec1a7db180d3ecaacc
Andreas Sefzig <andreas@sefzig.net>
2016-04-24 23:33:00 +0200

    Kontaktseite: [Text:SefzignetKontakt]

--- script.js
+++ script.js
@@ -162,9 +162,9 @@ module.exports = new Script({
                                                               +'\n○ --kontaktTwitter '
                                                               +'\n○ --kontaktSlack '); }
             
-            if (befehl == "--KONTAKT")        { bot.say(SefzigBot+'Andreas E-Mail-Adresse: [Button:andreas@sefzig.net,mailto:andreas@sefzig.net] Kontaktseite: [Text:SefzignetKontakt]');
-                                                bot.say(SefzigBot+'Oder rufen Sie ihn einfach an: [Button:0151 15 92 00 82,tel:+4915115920082]');
-                                                bot.say(SefzigBot+'Besuchen Sie ihn auch auf --Twitter und --Xing oder treten Sie seinem offenen --Slackteam bei!'); }
+            if (befehl == "--KONTAKT")        { bot.say(SefzigBot+'Andreas E-Mail-Adresse: [Button:andreas@sefzig.net,mailto:andreas@sefzig.net] Andreas Kontaktdaten: [Text:SefzignetKontakt]');
+                                                bot.say(SefzigBot+'Rufen Sie Andreas an! [Button:0151 15 92 00 82,tel:+4915115920082]');
+                                                bot.say(SefzigBot+'Besuchen Sie Andreas auf --Twitter und --Xing oder treten Sie seinem offenen --Slackteam bei!'); }
             if (befehl == "--SLACKTEAM")      { bot.say(SefzigBot+'Andreas betreibt ein offenes Slack-Team, in dem man sich über Marketing-Technologie unterhält. %[Slack-Team öffnen](http://sefzig.net/link/SlackAnmeldung/)'); }
             if (befehl == "--XING")           { bot.say(SefzigBot+'Andreas ist bei Xing: Er ist seit 2004 angemeldet und gehört mit über 500 Kontakten zu den 5% der bestvernetzten Mitglieder. %[Xing-Profil öffnen](http://sefzig.net/link/XingProfil/)'); 
                                                 bot.say(SefzigBot+'[Bild:http://sefzig.net/text/seiten/SefzigBot/dateien/xing_vernetzt.png]'); }
