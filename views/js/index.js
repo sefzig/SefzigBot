ac08d8e0f8047aec2d3cc88675411d526962ed8d
Andreas Sefzig <andreas@sefzig.net>
2016-04-24 17:43:25 +0200

    parentparent

--- views/js/index.js
+++ views/js/index.js
@@ -232,8 +232,8 @@
           }
           
           var ist_erster = $(".sk-from.bot"+zufall).parent().parent().prev().hasClass("sk-right-row");
-          if (ist_erster != true) { $(".sk-from.bot"+zufall).css("display","none").css("padding-top","0px"); }
-          console.log('> ist_erster: '+ist_erster);
+          if (ist_erster != true) { $(".sk-from.bot"+zufall).css("display","none"); $(".sk-from.bot"+zufall).parent().parent().css("padding-top","10px"); }
+          console.log('> ist_erster: '+ist_erster); 
           ist_erster = "";
           
        }
