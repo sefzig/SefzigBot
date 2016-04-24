c56b8aebfe414b810010a0e9089e9c983dbb6010
Andreas Sefzig <andreas@sefzig.net>
2016-04-24 07:36:19 +0200

    Tests

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
+app.use("/img", express.static(__dirname + '/views/img'));
 app.use("/css", express.static(__dirname + '/views/css'));
 app.use("/js", express.static(__dirname + '/views/js'));
 
