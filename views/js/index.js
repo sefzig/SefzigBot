baf8b0dc7ea4f2d064fc683cfd29e8c79f755a2b
Andreas Sefzig <andreas@sefzig.net>
2016-04-25 11:43:55 +0200

    Robogeddon

--- views/js/index.js
+++ views/js/index.js
@@ -158,7 +158,7 @@
        // Templates
           var text =    '<div class="sk-action" style="margin-bottom:-20px"><a class="btn btn-sk-primary" href="http://sefzig.net/text/%inhalt%/" onclick="texte(\'%inhalt%\'); return false;">Text: %inhalt%</a></div>';
           var button =  '<div class="sk-action" style="margin-bottom:0px"  ><a class="btn btn-sk-primary" href="'+button_url+'" target="_blank">'+button_text+'</a></div>';
-          var iframe =  '<iframe src="" data-src="%inhalt%" width="180" height="102" onclick="this.src = this.data-src" frameborder="0">Frame laden</iframe>';
+          var iframe =  '<iframe src="%inhalt%" width="180" height="102" frameborder="0">Frame laden</iframe>';
           var bild =    '<img class="%klasse%" src="%inhalt%" />';
           var youtube = '<iframe width="180" height="102" class="%klasse%" src="http://www.youtube.com/embed/%inhalt%?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
           
