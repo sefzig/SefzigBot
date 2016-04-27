6624e2f0766a4881a3ee89b39bea73c859dc71c3
Andreas Sefzig <andreas@sefzig.net>
2016-04-27 18:30:29 +0200

    Bots

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ module.exports = new Script({
  // -------------------------
  // Register
  // -------------------------
-         
+    
     register: {
         receive: (bot, message) => {
             
@@ -92,18 +92,6 @@ module.exports = new Script({
          // -----------------
          // Bots
          // -----------------
-            
-            if ((~befehl.indexOf("--LINK")) ||
-                (~befehl.indexOf("--LINKS")))         { bot.say(LinkBot  +'Andreas speichert viele interessante Links - für sich wie auch für Sie: [Linkliste:Allgemein] 1.000 Links, 6.000 Aufrufe!');
-                                                        bot.say(LinkBot  +'Steuern Sie mich mit diesen Befehlen: \n○ --Links \n○ --Listen \n○ --Liste \n○ --Eingabe \n○ --Einrichten \n○ --Admin \n○ --Uber');
-                                                        bot.say(LinkBot  +'(Schreiben Sie --bot, um wieder mit SefzigBot zu sprechen.)');
-                                                        dann = "link"; } 
-            
-            if ((~befehl.indexOf("--TEXT")) ||
-                (~befehl.indexOf("--TEXTE")))         { bot.say(TextBot  +'Andreas verwaltet, bearbeitet und teilt alle seine Texte mit einer eigens entwickelten Software: [Text:Text] ');
-                                                        bot.say(TextBot  +'(Schreiben Sie --bot, um wieder mit SefzigBot zu sprechen.)');
-                                                        bot.say(TextBot  +'Steuern Sie mich mit diesen Befehlen: \n○ --Startseite \n○ --Hilfe \n○ --Uber');
-                                                        dann = "text"; } 
             
             if  (~befehl.indexOf("--SLACK"))          { bot.say(SlackBot +'Andreas mag Interne Kommunikation und Automatisierung - er liebt Slack! Das sollte Ihr Team auch tun...');
                                                         bot.say(SlackBot +'(Schreiben Sie --bot, um wieder mit SefzigBot zu sprechen.)');
@@ -126,77 +114,6 @@ module.exports = new Script({
             if  (~befehl.indexOf("--MOBIL"))          { bot.say(SefzigBot+'Diesen Chat mobil öffnen: [Bild:https://zxing.org/w/chart?cht=qr&chs=200x200&chld=L&choe=UTF-8&chl=http%3A%2F%2Fsefzigbot.herokuapp.com%2F]');
                                                         bot.say(SefzigBot+'(Leider werden Sie dort nicht wiedererkannt. Das sollte in einer späteren Version möglich sein...)'); }
             if  (~befehl.indexOf("--NAME"))           { dann = "name"; }
-            
-         // -----------------
-         // Person
-         // -----------------
-         
-            if  (~befehl.indexOf("--BEFEHLE"))        { bot.say(SefzigBot+'--Person '
-                                                              +'\n○ --Über '
-                                                              +'\n○ --Foto '
-                                                              +'\n○ --Lebenslauf '
-                                                              +'\n○ --Kompetenzen '
-                                                              +'\n○ --Disziplinen '); }
-            
-            if  (~befehl.indexOf("--PERSON"))         { bot.say(SefzigBot+'Ich hätte da Andreas --Lebenslauf, eine Liste seiner --Kompetenzen und eine Liste der Marketing --Disziplinen, die er beherrscht.');
-                                                        bot.say(SefzigBot+'[Bild:http://sefzig.net/text/seiten/SefzignetStartseite/dateien/SefzignetStartseiteFotoLogos.png]');
-                                                        bot.say(SefzigBot+'Andreas ist 38 Jahre alt und lebt in Hamburg.'); }
-            if  (~befehl.indexOf("--LEBENSLAUF"))     { bot.say(SefzigBot+'Sein Lebenslauf als druckbares PDF-Dokument: %[Lebenslauf.pdf](http://sefzig.net/text/seiten/Lebenslauf/dateien/Andreas_Sefzig_Lebenslauf.pdf)');
-                                                        bot.say(SefzigBot+'Tabellarisches und ausformuliertes CV: %[Lebenslauf öffnen](http://sefzig.net/link/Lebenslauf/)'); }
-            if  (~befehl.indexOf("--KOMPETENZEN"))    { bot.say(SefzigBot+'Text Kompetenzen: [Text:Kompetenzen] '); }
-            if  (~befehl.indexOf("--DISZIPLINEN"))    { bot.say(SefzigBot+'Text Disziplinen: [Text:Disziplinen] '); }
-            if  (~befehl.indexOf("--FOTO"))           { bot.say(SefzigBot+'[Bild:http://sefzig.net/text/seiten/SefzignetStartseite/dateien/SefzignetStartseiteFotoLogos.png] Andreas Sefzig mit Logo'); }
-            
-         // -----------------
-         // Neues
-         // -----------------
-         
-         // Übersicht
-            if  (~befehl.indexOf("--BEFEHLE"))        { bot.say(SefzigBot+'--neues '
-                                                              +'\n○ --jobsuche '
-                                                              +'\n○ --slack '
-                                                              +'\n○ --rtm '); }
-            
-         // Befehle
-            if  (~befehl.indexOf("--NEUES"))          { bot.say(SefzigBot  +'○ --jobsuche '
-                                                              +'\n○ --slack / --rtm '
-                                                              +'\n○ --newsletter ');
-                                                        bot.say(SefzigBot  +' Was es bei Andreas --Neues gibt? So einiges... '); }
-            
-            if ((~befehl.indexOf("--FRISKY")) ||
-                (~befehl.indexOf("--RADIO")) ||
-                (~befehl.indexOf("--FRISKYRADIO")))   { bot.say(SefzigBot+'Frisky Radio: Global EDM [Audio:http://www.friskyradio.com/m3u/frisky.m3u] Mitglied seit 2003.'); }
-            
-            if  (~befehl.indexOf("--ROBOGEDDON"))     { bot.say(SefzigBot+'#Robogeddon ist Andreas Hashtag für den Einzug von Chatbots in unser Leben. [Text:Robogeddon] Bots werden die digitale Welt verändern!'); }
-            if  (~befehl.indexOf("--SLACKTEAM"))      { bot.say(SefzigBot+'Treten Sie dem offenen Slack-Team von Andreas bei, um sich mit ihm zu beraten und um Slack im laufenden Betrieb zu sehen: [Button:Anmeldung,http://sefzig.net/link/SlackAnmeldung/] '); }
-            
-         // -----------------
-         // Kontakt 
-         // -----------------
-         
-            if  (~befehl.indexOf("--BEFEHLE"))        { bot.say(SefzigBot+'--kontakt '
-                                                              +'\n○ --kontaktSeite '
-                                                              +'\n○ --kontaktTwitter '
-                                                              +'\n○ --kontaktSlack '); }
-            
-            if  (~befehl.indexOf("--KONTAKT"))        { bot.say(SefzigBot+'Andreas mobile Telefonnummer: [Button:0151 - 15 92 00 82,tel:+4915115920082] Andreas E-Mail-Adresse: [Button:andreas@sefzig.net,mailto:andreas@sefzig.net] Weitere Kontaktdaten: [Text:SefzignetKontakt]');
-                                                        bot.say(SefzigBot+'Besuchen Sie Andreas auf --Twitter und --Xing oder treten Sie seinem offenen --SlackTeam bei!'); }
-            if  (~befehl.indexOf("--SLACKTEAM"))      { bot.say(SefzigBot+'Andreas betreibt ein offenes Slack-Team, in dem man sich über Marketing-Technologie unterhält. %[Slack-Team öffnen](http://sefzig.net/link/SlackAnmeldung/)'); }
-            if  (~befehl.indexOf("--XING"))           { bot.say(SefzigBot+'Andreas ist bei Xing: Er ist seit 2004 angemeldet und gehört mit über 500 Kontakten zu den 5% der bestvernetzten Mitglieder. %[Xing-Profil öffnen](http://sefzig.net/link/XingProfil/)'); 
-                                                        bot.say(SefzigBot+'[Bild:http://sefzig.net/text/seiten/SefzigBot/dateien/xing_vernetzt.png]'); }
-            if  (~befehl.indexOf("--TWITTER"))        { bot.say(SefzigBot+'Andreas ist auf Twitter: Er ist seit 2008 angemeldet, hat beinahe 500 Follower und sah sich kürzlich gezwungen, alle Tweets zu löschen: Neustart 2016. %[Twitter-Profil öffnen](http://sefzig.net/link/TwitterProfil/)'); 
-                                                        bot.say(SefzigBot+'Schreiben Sie einen Tweet über @sefzig: %[@sefzig twittern](http://sefzig.net/link/TwitterTweet/)'); }
-            
-         // -----------------
-         // Simon the cat
-         // -----------------
-            
-            if  (~befehl.indexOf("--MAUNZ"))          { bot.say(SefzigBot+'Befehle: --maunz: --maudreh, --maukuschel, --maubox, --maukugel, --mauohr'); }
-            if  (~befehl.indexOf("--MAUDREH"))        { bot.say(SefzigBot+'[Bild:://49.media.tumblr.com/14633d451a8cb2642fd644fdd9a0a319/tumblr_meqaedpljZ1qal0zgo1_r1_500.gif ]'); }
-            if  (~befehl.indexOf("--MAUKUSCHEL"))     { bot.say(SefzigBot+'[Bild:://2.bp.blogspot.com/-A0RNd-nRhyc/UDfcMvTV-MI/AAAAAAAAC6M/cKd3J47vqmg/s1600/tumblr_lvqjn0woFq1qerhg8o1_500_large.gif]'); }
-            if  (~befehl.indexOf("--MAUBOX"))         { bot.say(SefzigBot+'[Bild:://ic.pics.livejournal.com/akira_001/66442224/134158/134158_900.gif]'); }
-            if  (~befehl.indexOf("--MAUKUGEL"))       { bot.say(SefzigBot+'[Bild:://media.giphy.com/media/NFRXs0b3DyIve/giphy.gif]'); }
-            if  (~befehl.indexOf("--MAUOHR"))         { bot.say(SefzigBot+'[Bild:://media.giphy.com/media/k03ZWbT5M7QfC/giphy.gif]'); }
             
          // -----------------
          // Tests
@@ -288,70 +205,6 @@ module.exports = new Script({
           bot.say(LinkBot+`Der Json-Feed von "${kurzel}": http://sefzig.net/link/liste/${kurzel}/json/.`)) .then(() => 'link');
        }
     },
-    
- // -------------------------
- // Text
- // -------------------------
-    
-    text: {
-        receive: (bot, message) => {
-            
-         // Befehl normalisieren
-            var befehl = message.text.trim().toUpperCase();
-            var befehlname = message.text.trim().toLowerCase().replace("--", "");
-            
-         // Nächster Schritt default
-            var dann = "text";
-            
-         // Befehle
-            if ((~befehl.indexOf("--BEFEHLE")) ||
-                (~befehl.indexOf("--TEXT")))          { bot.say(TextBot  +'--Text '
-                                                              +'\n○ --Startseite '
-                                                              +'\n○ --Hilfe '
-                                                              +'\n○ --URLs '
-                                                              +'\n○ --Über');
-                                                        bot.say(TextBot  +'Andreas schreibt interessante Texte für sich und andere: %[Linkliste öffnen](http://sefzig.net/link/liste/Text/)'); }
-            if ((~befehl.indexOf("--BOT")) ||
-                (~befehl.indexOf("--ABBRECHEN")))     { bot.say(TextBot  +'Zurück an Sefzig --Bot. Bis später!');
-                                                        dann = "register"; }
-            
-         // Inhalte
-            if  (~befehl.indexOf("--STARTSEITE"))     { bot.say(TextBot  +'Auf der Startseite können bestehende Texte geöffnet oder ein neuer Text angelegt werden: %[Startseite öffnen](http://sefzig.net/text/)'); }
-            if  (~befehl.indexOf("--HILFE"))          { bot.say(TextBot  +'Die Hilfeseite erklärt Text, die Wikisprache Textile und die verfügbaren Inhalts-Module: %[Hilfe öffnen](http://sefzig.net/text/)'); }
-            if  (~befehl.indexOf("--URLS"))           { bot.say(TextBot  +'Die --urls der verschiedenen Ansichten eines Texts generieren: --extern, --einbindung, --ausgabe, --eingabe, --editor, --dateien, --html, --mobil'); }
-            if  (~befehl.indexOf("--UBER"))           { bot.say(TextBot  +'"Text" ist eine Web-Anwendung zur Verwaltung von Texten wie Notizen, Artikel, Whitepapers, Wikis und sogar Präsentationen.');
-                                                        bot.say(TextBot  +'Hier die Dokumentation: [Text:text] ');
-                                                        bot.say(TextBot  +'Andreas hat Text entwickelt, um sich und seinem Umfeld einen leichteren Umgang mit Texten zu verschaffen.'); }
-            
-         // URL-Generator
-            if  (~befehl.indexOf("--AUSGABE"))        { dann = "text_ausgabe"; }
-            if  (~befehl.indexOf("--DATEIEN"))        { dann = "text_dateien"; }
-            if  (~befehl.indexOf("--EDITOR"))         { dann = "text_editor"; }
-            if  (~befehl.indexOf("--EINBINDUNG"))     { dann = "text_einbindung"; }
-            if  (~befehl.indexOf("--EINGABE"))        { dann = "text_eingabe"; }
-            if  (~befehl.indexOf("--EXTERN"))         { dann = "text_extern"; }
-            if  (~befehl.indexOf("--HTML"))           { dann = "text_html"; }
-            if  (~befehl.indexOf("--MOBIL"))          { dann = "text_mobil"; }
-            if  (~befehl.indexOf("--SOLOMO"))         { dann = "text_solomo"; }
-            
-         // Konversation fortführen
-            return bot.setProp('text', 'gesprochen')
-                .then(() => dann);
-            
-        }
-        
-    },
-    
- // URL-Generator 
-    text_ausgabe:    { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Ausgabe-URL     zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_ausgabe',    kurzel) .then(() => bot.say(TextBot+`Die Ausgabe     von "${kurzel}": http://sefzig.net/text/${kurzel}/#ausgabe.`))    .then(() => 'text'); } },
-    text_dateien:    { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Dateien-URL     zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_dateien',    kurzel) .then(() => bot.say(TextBot+`Die Dateien     von "${kurzel}": http://sefzig.net/text/${kurzel}/#dateien.`))    .then(() => 'text'); } },
-    text_editor:     { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Editor-URL      zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_editor',     kurzel) .then(() => bot.say(TextBot+`Die Editor      von "${kurzel}": http://sefzig.net/text/${kurzel}/#editor.`))     .then(() => 'text'); } },
-    text_einbindung: { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Einbindungs-URL zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_einbindung', kurzel) .then(() => bot.say(TextBot+`Die Einbindungs von "${kurzel}": http://sefzig.net/text/${kurzel}/#einbindung.`)) .then(() => 'text'); } },
-    text_eingabe:    { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Eingabe-URL     zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_eingabe',    kurzel) .then(() => bot.say(TextBot+`Die Eingabe     von "${kurzel}": http://sefzig.net/text/${kurzel}/#eingabe.`))    .then(() => 'text'); } },
-    text_extern:     { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Extern-URL      zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_extern',     kurzel) .then(() => bot.say(TextBot+`Die Extern      von "${kurzel}": http://sefzig.net/text/${kurzel}/#extern.`))     .then(() => 'text'); } },
-    text_html:       { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Html-URL        zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_html',       kurzel) .then(() => bot.say(TextBot+`Die Html        von "${kurzel}": http://sefzig.net/text/${kurzel}/#html.`))       .then(() => 'text'); } },
-    text_mobil:      { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Mobil-URL       zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_mobil',      kurzel) .then(() => bot.say(TextBot+`Die Mobil       von "${kurzel}": http://sefzig.net/text/${kurzel}/#mobil.`))      .then(() => 'text'); } },
-    text_solomo:     { prompt: (bot) => bot.say(TextBot+'Schreiben Sie das Kürzel des Texts, um dessen Solomo-URL      zu generieren!'), receive: (bot, message) => { var kurzel = message.text; return bot.setProp('text_solomo',     kurzel) .then(() => bot.say(TextBot+`Die Solomo      von "${kurzel}": http://sefzig.net/text/${kurzel}/solomo/.`))     .then(() => 'text'); } },
     
  // -------------------------
  // Slack
