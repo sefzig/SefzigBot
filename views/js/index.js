534567a93a3d0abb3fb81670cbd610ee538767b8
Andreas Sefzig <andreas@sefzig.net>
2016-04-29 22:19:57 +0200

    sendMessage

--- views/js/index.js
+++ views/js/index.js
@@ -369,11 +369,12 @@
  // Klicks auf Befehle
     function befehlen(befehl) {
        
-       $("#sk-footer form input.message-input").val(befehl);
-       $("#sk-footer form input.message-input").change();
-       $("#sk-footer form input.message-input").focus();
+    // $("#sk-footer form input.message-input").val(befehl);
+    // $("#sk-footer form input.message-input").change();
+    // $("#sk-footer form input.message-input").focus();
     // $("#sk-footer form .send").trigger("click");
-       window.Smooch.sendMessage('my text');
+       
+       window.Smooch.sendMessage(befehl);
        
     }
     
