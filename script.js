b6013621e06e96f11866fe3ab0a7727d6cd0ad3e
Andreas Sefzig <andreas@sefzig.net>
2016-04-30 10:48:51 +0200

    versuch

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ module.exports = new Script({
     	
         receive: (bot) => {
             return bot.say(EmpfangsBot+'Darf ich Ihnen kurz unsere Agentur vorstellen? Dann schreiben (oder klicken) Sie bitte --Agentur!')
-                .then(() => bot.say(EmpfangsBot+'Ich möchte Ihnen auch unsere --Kreation, die --Beratung und unsere --Technik vorstellen. <br /> Oder lassen Sie uns über --Strategie und --Konzeption reden.'))
+                .then(() => bot.say(EmpfangsBot+'Ich möchte Ihnen auch unsere --Kreation, die --Beratung und unsere --Technik vorstellen. \n Oder lassen Sie uns über --Strategie und --Konzeption reden.'))
                 .then(() => bot.say(AndreasSefzig+'Ich bin gerade nicht online. Lassen Sie mich benachrichtigen, indem Sie --Sefzig schreiben!'))
                 .then(() => 'register'); /* <-- vorname: automatisches Onboarding */
         }
@@ -188,9 +188,9 @@ module.exports = new Script({
          // Agentur
          // -----------------
             
-            if  (~befehl.indexOf("--AGENTUR"))        { versuch = true; bot.say(EmpfangsBot+'#Robogeddon ist auf Bots für externe und interne Unternehmens-Kommunikation spezialisiert.').then(function(){ return
-                                                                        bot.say(EmpfangsBot+'Wir bestehen aus Andreas Sefzig, einer Reihe moderner Marketing- und Kommunikations-Technologien und einem Team aus mehreren Bots.') }).then(function(){ return 
-                                                                        bot.say(EmpfangsBot+'Mehr über uns: --Folgt.'); }); }
+            if  (~befehl.indexOf("--AGENTUR"))        { versuch = true; bot.say(EmpfangsBot+'#Robogeddon ist auf Bots für externe und interne Unternehmens-Kommunikation spezialisiert.').then(() => 
+                                                                        bot.say(EmpfangsBot+'Wir bestehen aus Andreas Sefzig, einer Reihe moderner Marketing- und Kommunikations-Technologien und einem Team aus mehreren Bots.').then(() => 
+                                                                        bot.say(EmpfangsBot+'Mehr über uns: --Folgt.')); }
             
          // -----------------
          // Strategie
