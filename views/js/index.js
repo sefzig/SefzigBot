b1928a57fa8ecf07959722b32e8d0822775d57d5
Andreas Sefzig <andreas@sefzig.net>
2016-05-07 01:12:23 +0200

    tests

--- views/js/index.js
+++ views/js/index.js
@@ -185,23 +185,24 @@
           
        // Cta-Text von URL trennen
           if ((modul == "Button") || (modul == "Text") || (modul == "Link") || (modul == "Linkliste"))  { 
+             
           // console.log("> Button Var: "+var1);
              var buttons = text_string.split("["+modul+":");
              if (buttons[1]) { 
                 var buttons2 = buttons[1].split("]");
                 if (buttons2[1]) { 
                    var buttons3 = buttons2[0].split(",");
-                   if (buttons3[1]) { 
-                      var button_text = buttons3[0];
-                      var button_url = buttons3[1];
+                   if (buttons3[1]) { var button_text = buttons3[0];   var button_url = ""+buttons3[1];
                    }
-                   else { var button_text = "Link öffnen"; var button_url = ""+buttons2[0]; }
+                   else {             var button_text = "Link öffnen"; var button_url = ""+buttons2[0]; }
                 }
-                else {    var button_text = "Link öffnen"; var button_url = ""+var2; }
+                else {                var button_text = "Link öffnen"; var button_url = ""+var1; }
              }
-             else {       var button_text = "Link öffnen"; var button_url = ""+var1; }
-          // console.log("> Button Text: "+button_text);
-          // console.log("> Button Url: "+button_url);
+             else {                   var button_text = "Link öffnen"; var button_url = ""+var1; }
+             
+             console.log("> Button Text: "+button_text);
+             console.log("> Button Url: "+button_url);
+             
           }
           
        // Template laden und anpassen
